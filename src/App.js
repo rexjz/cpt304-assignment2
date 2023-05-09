e1505da4c44c927f99e8cffcca19dd92f7ff74e5
rexjz <robbort.xu@gmail.com>
2023-05-09 19:34:38 +0800

    holiday-viewer

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import CitySelector from "./components/CitySelector"
 import WheatherViewer from "./components/WhetherViewer"
 import { getWheather } from './apis';
 import { useState } from 'react';
+import HolidayViewer from './components/HolidayViewer'
 
 function App() {
   const [region, setRegion] = useState(undefined)
-  const [time, setTime] = useState('')
+  const [time, setTime] = useState(undefined)
   return (
-    <div className="App">
+    <div className="App" style={{padding: '32px'}}>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
           <div style={{ display: 'flex', flexDirection: 'column' }}>      
             <CitySelector onChange={setRegion}/>
+            <HolidayViewer region={region}/>
           </div>
-          <WheatherViewer time='2019-1-1' geo={region} datasource={getWheather} adapter={(apiResponse) => { return apiResponse}}/>
+          <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '12px' }}>
+            <WheatherViewer time='2019-1-1' geo={region} datasource={getWheather} adapter={(apiResponse) => { return apiResponse}}/> 
+          </div>
         </div>
     </div>
   );
---
+++ src/components/HolidayViewer/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+export default function HolidayViewer(props) {
+  const { region } = props
+  return (
+    <div className="holiday-viewer-container">
+      HolidayViewer
+    </div>
+  )
+}
